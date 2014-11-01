918c4ba7483cf8f4459f08305b02c8760b949def
Forrest Parker <forresta.parker@gmail.com>
2014-11-01 11:28:15 -0700

    tests written and properly failing

---
+++ README.md
@@ -0,0 +1,8 @@
+# How To
+
+## This is an exercise in nesting resources.  I wish to make a list that
+will have steps listed under it.  We can accomplish this by using basic
+scaffolding of both lists and steps and then modifying the routes file.
+
+## Contributions:
+http://guides.rubyonrails.org/routing.html
---
+++ test/features/how_to_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+feature 'List page' do
+  scenario 'should make list' do
+    visit root_path
+
+    click_on 'New List'
+    fill_in 'Title', with: 'Grocery Store'
+    click_on 'Create List'
+    page.must_have_content 'List was successfully created.'
+  end
+
+  scenario 'should write step in list' do
+    visit '/lists/1/steps'
+    fill_in 'Body', with: 'Tomatoes'
+    click_on 'Create Step'
+    page.must_have_content 'Step was successfully created'
+  end
+end
